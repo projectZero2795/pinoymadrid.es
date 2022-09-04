fc8d704b09ac142cb946509ab209e89e461a0a8e
projectZero2795 <projectZero2795@gmail.com>
2022-09-04 13:17:39 +0200

    simplify flag with gradient color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,42 +8,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'pinoymadrid.es';
   borderWidth = (window.innerWidth/2).toString() + "px";
-  borderTop  = (window.innerHeight/2).toString() + "px";
-  leftColor = "#ff0000";
-  rightColor = "#0000ff";
+  borderTop  = window.innerHeight.toString() + "px";
+  leftColor = "red linear-gradient(red, #ff0066)";
+  leftTriangle = "polygon(0% 0%, 0% 100%, 100% 100%, 100% 50%)";
+  rightTriangle = "polygon(0% 50%,100% 0%, 100% 100%, 0% 100%)";
+  rightColor = "blue linear-gradient(blue, #cc99ff)";
 
   topPhpFlagTriangle() {
     return {
-      'width': '0', 
-      'height':'0', 
-      'border-left': this.borderWidth + " solid transparent", 
-      'border-right': this.borderWidth + " solid transparent", 
-      'border-top': this.borderTop + " solid #336BFF", 
+      'width': (window.innerWidth).toString() + "px",
+      'height': this.borderTop,
+      'background': "#ffffe6 linear-gradient(#ffffe6, #b3b300)",
+      'clip-path':  "polygon(0% 0%,49% 50%, 100% 0%)",
       'position': 'absolute',
+      'left': 0,
     }
   }
+
 
   phpFlagTriangle(left:boolean) {
     return {
-      'width': '0', 
-      'height':'0',
-      'border-left': left ? 0 : this.borderWidth + " solid transparent", 
-      'border-right': left ? this.borderWidth + " solid transparent" : 0,
-      'border-bottom': this.borderTop + " solid " + (left ? this.leftColor : this.rightColor),
+      'width': this.borderWidth,
+      'height': this.borderTop,
+      'background': left ? this.leftColor : this.rightColor,
+      'clip-path': left ? this.leftTriangle : this.rightTriangle,
       'position': 'absolute',
-      'top':'0',
-      'left': left ? 0 :  this.borderWidth,
-    }
-  }
-
-  bottomPhpFlagSquare(left:boolean) {
-    return {
-      'width': '0', 
-      'height':'0',
-      'border': this.borderWidth + " solid " + (left ? this.leftColor : this.rightColor) ,
-      'position': 'absolute',
-      'top':this.borderTop,
-      'left': left ? 0 :  this.borderWidth,
+      'left': left ? 0 : this.borderWidth,
     }
   }
 }
