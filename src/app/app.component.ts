969c71b35f18d2ce78cc2999ce6accb1a2e551fa
projectZero2795 <projectZero2795@gmail.com>
2022-09-04 12:24:00 +0200

    flag structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pinoymadrid.es';
-  windowsWitdh = (window.innerWidth/2.3).toString() + "px solid transparent";
-  windowesHeight  = (window.innerHeight/2.5).toString() + "px solid #336BFF";
+  borderWidth = (window.innerWidth/2).toString() + "px";
+  borderTop  = (window.innerHeight/2).toString() + "px";
+  leftColor = "#ff0000";
+  rightColor = "#0000ff";
+
+  topPhpFlagTriangle() {
+    return {
+      'width': '0', 
+      'height':'0', 
+      'border-left': this.borderWidth + " solid transparent", 
+      'border-right': this.borderWidth + " solid transparent", 
+      'border-top': this.borderTop + " solid #336BFF", 
+      'position': 'absolute',
+    }
+  }
+
+  phpFlagTriangle(left:boolean) {
+    return {
+      'width': '0', 
+      'height':'0',
+      'border-left': left ? 0 : this.borderWidth + " solid transparent", 
+      'border-right': left ? this.borderWidth + " solid transparent" : 0,
+      'border-bottom': this.borderTop + " solid " + (left ? this.leftColor : this.rightColor),
+      'position': 'absolute',
+      'top':'0',
+      'left': left ? 0 :  this.borderWidth,
+    }
+  }
+
+  bottomPhpFlagSquare(left:boolean) {
+    return {
+      'width': '0', 
+      'height':'0',
+      'border': this.borderWidth + " solid " + (left ? this.leftColor : this.rightColor) ,
+      'position': 'absolute',
+      'top':this.borderTop,
+      'left': left ? 0 :  this.borderWidth,
+    }
+  }
 }
